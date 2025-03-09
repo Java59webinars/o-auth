821d7e74a69b8e694ee39b28aefcb37eafb1658f
MatveyRomodanovskiy <startpolus@gmail.com>
2025-03-09 22:40:21 +0300

    Initial structure with firebase auth

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -5,14 +5,7 @@ import {getAuth, GoogleAuthProvider, signInWithPopup, signOut} from "firebase/au
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyBRnokOFSEC-WkqdsG2t3qV_atnMG2DgAY",
-    authDomain: "o-auth2-88866.firebaseapp.com",
-    projectId: "o-auth2-88866",
-    storageBucket: "o-auth2-88866.firebasestorage.app",
-    messagingSenderId: "922749857300",
-    appId: "1:922749857300:web:e116d15984101c326d0101"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
