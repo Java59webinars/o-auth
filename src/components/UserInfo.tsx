5a02b6a8ede2678cbca2298d7a5c42b625a5f4d7
MatveyRomodanovskiy <startpolus@gmail.com>
2025-03-16 12:23:26 +0300

    connect redux to UserInfo

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,11 +1,21 @@
 import {Avatar, Box, Typography} from "@mui/material";
-
+import { themeStyles } from "../services/theme";
+import {useAppSelector} from "../redux/store.ts";
 
 const UserInfo = () => {
+    const user = useAppSelector((state) => state.auth.user);
+
+    if (!user) {
+        return null; // Если пользователь не авторизован, не показываем компонент
+    }
     return (
         <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-            <Avatar sx={{ width: 50, height: 50 }} />
-            <Typography variant="h6">UserName</Typography>
+            <Avatar
+                src={user.photoURL || undefined}
+                alt={user.displayName || "Пользователь"}
+                sx={themeStyles.avatar}
+            />
+            <Typography variant="h6">{user.displayName }</Typography>
         </Box>
     );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,5 @@
 import {configureStore} from "@reduxjs/toolkit"
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector, TypedUseSelectorHook} from "react-redux";
 import authReducer from "./authSlice";
 export const store = configureStore({
     reducer: {
@@ -11,3 +11,4 @@ export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
